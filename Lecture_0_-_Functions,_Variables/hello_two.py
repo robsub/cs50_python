0f055941b4c5df245b83e789cae678af5b6f1e06
Rob Field <rob@robs-air.home>
2023-01-06 15:27:01 +0000

    sum

--- Lecture_0_-_Functions,_Variables/hello_two.py
+++ Lecture_0_-_Functions,_Variables/hello_two.py
@@ -109,5 +109,19 @@ main()
 # NameError: name 'name' is not defined 
 
 
-#
+#Returning Values
+# You can imagine many scenarios where you don’t just want a function to perform an action, but also to return a value back to the main function. For example, rather than simply printing the calculation of x + y, you may want a function to return the value of this calculation back to another part of your program. This “passing back” of a value we call a return value.
+# Returning to our calculator.py code by typing code calculator.py. Erase all code there. Rework the code as follows:
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+
+def square(n):
+    return n * n
+
+
+main()
+# Effectively, x is passed to square. Then, the calculation of x * x is returned back to the main function.
 
---
+++ Lecture_0_-_Functions,_Variables/summing_up.py
@@ -0,0 +1,47 @@
+# Summing Up
+# Through the work of this single lecture, you have learned abilities that you will use countless times in your own programs. You have learned about…
+
+# Creating your first programs in Python;
+# Functions;
+# Bugs;
+# Variables;
+# Comments;
+# Pseudocode;
+# Strings;
+# Parameters;
+# Formatted Strings;
+# Integers;
+# Principles of readability;
+# Floats;
+# Creating your own functions; and
+# Return values.
+
+
+# RETURN VALUES
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", RobsSquareeFunction(x))
+
+
+def RobsSquareeFunction(n):
+    return n * n
+
+
+main()
+
+
+
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", RobsMinus2Function(x))
+
+
+def RobsMinus2Function(n):
+    return n - 2
+
+
+main()
+
+
+# more on return values https://realpython.com/python-return-statement/
